8007327b9bbc5ea67b2ecd50d29cb39ef0ab6236
arunseege <arunsphassan@gmail.com>
2023-04-04 08:01:53 +0530

    created eventsource and wikimedia kafka producer

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
