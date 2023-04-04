d26d90248fac1a9b0a66527a05b7a6eb533bc584
arunseege <arunsphassan@gmail.com>
2023-04-04 11:45:56 +0530

    mysql configured and start to save events in database

--- kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.entity.WikiMediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,9 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+private WikimediaDataRepository repository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository repository) {
+        this.repository = repository;
+    }
 
     @KafkaListener(topics = "wikimedia_recentchange",groupId = "myGroup")
     public void consume(String  eventMessage){
         LOGGER.info(String.format("Event Message received --> %s",eventMessage));
+        WikiMediaData data = new WikiMediaData();
+        data.setWikiEventData(eventMessage);
+        repository.save(data);
     }
+
 }
